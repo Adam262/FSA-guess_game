10cdeade0437b160982a5487400daf19e113811e
Adam262 <abarcan@gmail.com>
2014-05-26 18:27:15 -0400

    big progress.  got heart of guessing game to work. you can play, with a few kinks.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,74 @@
 function q() {
     return Math.ceil(Math.random() * 100);
     }
+var countdown = 5;
+var random = q();
 
 $(document).ready(function(){
+
    $('#submit').on('click', function(){
-      q();
-      console.log (q());
+      random;
+      console.log (random);
+      
       var value =$('#guess').val();
       console.log (value); 
-   }); // handler for random number generator
+
+      countdown -=1;
+      if (countdown >=0) {
+        $('#counter').val(countdown).html(countdown);
+         $('#message').html("");
+      }
+      if (countdown ==0) {
+        $('#message').val("GAME OVER!").addClass("game_over");
+
+      }
+
+       if (value > 100 || value < 1 ) {
+            $('#message').html("That does not count. Enter a number beween 1 and 100.");
+                }
+            else if (isNaN (value)) {
+            $('#message').html("That does not count. Enter a number beween 1 and 100.");
+        } else if (random - value >= 50) {
+            $('#message').html("You're ice cold. Guess way higher.");
+        } else if (random - value < 50 && random - value >= 30) {
+            $('#message').html("You're quite cold. Guess higher.");
+        } else if (random - value < 30 && random - value >= 20) {
+            $('#message').html("You're getting there. Keep guessing higher.");
+        } else if (random - value < 20 && random - value >= 10) {
+            $('#message').html("You're warm. But still guess higher.");
+        } else if (random - value < 10 && random - value >= 5) {
+            $('#message').html("You're quite warm. Just guess a little higher.");
+        } else if (random - value < 5 && random - value > 0) {
+            $('#message').html("You're red hot. Guess a tad higher.");       
+        } else if (value - random >= 50) {
+            $('#message').html("You're ice cold. Guess way lower.");
+        } else if (value - random < 50 && value - random >= 30) {
+            $('#message').html("You're quite cold. Guess lower.");
+        } else if (value - random < 30 && value - random >= 20) {
+            $('#message').html("You're getting thee. Keep guessing lower.");
+        } else if (value - random < 20 && value - random >= 10) {
+            $('#message').html("You're warm. But still guess lower.");
+        } else if (value - random < 10 && value - random >= 5) {
+            $('#message').html("You're quite warm. Just guess a little lower.");
+        } else if (value - random < 5 && value - random > 0) {
+            $('#message').html("You're red hot. Guess a tad lower.");
+        } else if (value == random) {
+            $('#message').html("Congrats. You win!");
+                }
+    }); // handler for random number generator
+
+   $('#reset').on('click', function(){
+        $('#guess').val("Enter a number between  1  and  100").removeClass('game_over');
+        countdown = 5;
+        $('#counter').val(countdown).html(countdown);
+         $('#message').html("");
+
+    }); // handler for reset button
+
+     $('#hint').on('click', function(){
+         $('#message').html("You are within" + Math.abs(value - random)*2 + " units of the right answer");
+
+    }); // handler for hint button
 }); //document ready function
 
 
@@ -20,4 +80,12 @@ $(document).ready(function(){
         $(this).removeClass("blue");
     });
 
+*/
+
+/*
+ $('#guess').on('keypress', function(){
+      if (guess_val>100 || guess_val<0 || isNaN(guess_val)) {
+        alert('Please enter a number between 1 and 100');
+      }
+   });
 */
--- readme.md
+++ readme.md
@@ -5,16 +5,17 @@ Project Requirements:
 When a game begins, there should be a random number generated between 1-100. X
 The user should have an input field where they can submit a guess. X
 After the user submits a guess, indicate whether their guess is 'hot' or 'cold'. Let the user know if they need to guess higher or lower.
-Allow the user to guess only a certain amount of times. When they run out of guesses let them know the game is over.
+Allow the user to guess only a certain amount of times. X
+When they run out of guesses let them know the game is over. X
 Feel free to use prompts to get user input on your first version.
-For the final version of your project, you'll need to create an HTML-based interface for getting user inputs and giving feedback on guesses.
+For the final version of your project, you'll need to create an HTML-based interface for getting user inputs and giving feedback on guesses. X
 Validate inputs that they are real numbers between 1-100.
-Create a new game button that resets the game.
+Create a new game button that resets the game. X
 Store all of the guesses and create a way to check if the guess is a repeat.
 Track the user's previous guess. Let them know if they are getting “hotter” or “colder” based on their previous guess.
 
 Optional: 
 Create a button that provides the answer (Give me a Hint).
-Submit the guess by pressing enter or clicking the submit button.
+Submit the guess by pressing enter or clicking the submit button. X
 After a user guesses a number keep a visual list of Hot and Cold answers that the user can see.
 Change the background color, add an image, or do something creative when the user guesses the correct answer.
