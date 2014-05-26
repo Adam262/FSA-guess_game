b4bb8dcf5400680aeaf791800116c0bb6359b1ce
Adam262 <abarcan@gmail.com>
2014-05-25 21:12:32 -0400

    css style, random num generator, user number input capture

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
+function q() {
+    return Math.ceil(Math.random() * 100);
+    }
+
 $(document).ready(function(){
+   $('#submit').on('click', function(){
+      q();
+      console.log (q());
+      var value =$('#guess').val();
+      console.log (value); 
+   }); // handler for random number generator
+}); //document ready function
+
+
+/*
     $('h1').on('mouseenter', function(){
         $(this).addClass("blue");
     });
     $('h1').on('mouseleave', function(){
         $(this).removeClass("blue");
     });
-});
+
+*/
